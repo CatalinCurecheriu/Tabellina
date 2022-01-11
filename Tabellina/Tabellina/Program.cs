2c7374112335cf2c06b682c9c7d4164c80d89be1
CatalinCurecheriu <88787486+CatalinCurecheriu@users.noreply.github.com>
2022-01-11 17:12:37 +0100

    Add files via upload

---
+++ Tabellina/Tabellina/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Tabellina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int moltiplicatore = 1, i = 0;
+            int risultato;
+            Console.WriteLine("Inserisci un numero e ti stamperò la tabellina relativa");
+            int n_inserito = Convert.ToInt32(Console.ReadLine());
+
+
+            while (i <= 10)
+            {
+                risultato = n_inserito * i;
+                Console.WriteLine($"{n_inserito} x {i} =" + risultato);
+                i++;
+            }
+            i = 0;
+            Console.WriteLine();
+
+            //************************************************************************************
+
+            do
+            {
+               risultato = n_inserito * i;
+                Console.WriteLine($"{n_inserito} x {i} =" + risultato);
+                i++;              
+            } while (i <= 10);
+            Console.WriteLine();
+
+            //*****************************************************************************
+
+            for (i = 0; i <= 10; i++)
+            {
+                risultato = n_inserito * i;
+                Console.WriteLine($"{n_inserito} x {i} =" + risultato);
+                
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
